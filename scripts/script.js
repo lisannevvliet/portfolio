cf94a0a756a4aad586708dff94fa660f005ae9c0
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2023-12-03 12:42:09 +0100

    Load the repositories with client-side JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,53 @@ function $(element) {
 $("#filter").onchange = function() {
     $("#form").submit()
 }
+
+// Get all public lisannevvliet repositories.
+fetch("https://api.github.com/users/lisannevvliet/repos?per_page=100")
+    .then(response => response.json())
+    .then(data =>
+        data.forEach(item => {
+            html = `<section>
+            <h3>${item.name}</h3>`
+
+            if (item.description) {
+                html += `<p>${item.description}</p>`
+            }
+
+            html += `<span>
+            <a href="${item.url}" target="_blank"><img src="/images/github.png">GitHub</a>`
+
+            if (item.homepage) {
+                html += `<a href="${item.homepage}" target="_blank"><img src="/images/desktop-monitor.png">Demo</a>`
+            }
+
+            switch (item.language) {
+                case "CSS":
+                    html += `<a class="language"><img src="/images/css-3.png">${item.language}</a>`
+                    break;
+                case "JavaScript":
+                    html += `<a class="language"><img src="/images/java-script.png">${item.language}</a>`
+                    break;
+                case "EJS":
+                    html += `<a class="language"><img src="/images/ejs_icon_132422.png">${item.language}</a>`
+                    break;
+                case "Handlebars":
+                    html += `<a class="language"><img src="/images/handlebars_original_logo_icon_146483.png">${item.language}</a>`
+                    break;
+                case "HTML":
+                    html += `<a class="language"><img src="/images/html.png">${item.language}</a>`
+                    break;
+                case "Kotlin":
+                    html += `<a class="language"><img src="/images/pngwing.com.png">${item.language}</a>`
+                    break;
+                case "Swift":
+                    html += `<a class="language"><img src="/images/swift.png">${item.language}</a>`
+            }
+
+            html += `</span>
+            </section>`
+            
+            // Add the repositories to the index page.
+            $("section").insertAdjacentHTML("beforeend", html)
+        })
+    )
