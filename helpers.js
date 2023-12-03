d6a5f1bfa590bdeb7abbb4dce837576fc7afc4e3
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2023-12-03 11:38:30 +0100

    Delete npm files in root

--- helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ifCond: function(v1, v2, options) {
-        if (v1 === v2) {
-            return options.fn(this)
-        }
-        return options.inverse(this)
-      }
-}
