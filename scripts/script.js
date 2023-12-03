62386a7c0c6f4ab52511cf4b75b6637e822c0de1
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2023-12-03 14:04:15 +0100

    Add filter funtionality

--- scripts/script.js
+++ scripts/script.js
@@ -3,57 +3,87 @@ function $(element) {
     return document.querySelector(element)
 }
 
-// Submit the form upon a change in the filter dropdown.
-$("#filter").onchange = function() {
-    $("#form").submit()
+// Change the repositories upon a change in the filter dropdown.
+$("select").onchange = event => {
+    $("section").replaceChildren()
+    get(event.target.value)
 }
 
-// Get all public lisannevvliet repositories.
-fetch("https://api.github.com/users/lisannevvliet/repos?per_page=100")
-    .then(response => response.json())
-    .then(data =>
-        data.forEach(item => {
-            html = `<section>
-            <h3>${item.name}</h3>`
+get("all")
 
-            if (item.description) {
-                html += `<p>${item.description}</p>`
-            }
+function get(filter) {
+    // Get all public lisannevvliet repositories.
+    fetch("https://api.github.com/users/lisannevvliet/repos?per_page=100&sort=updated&direction=asc")
+        .then(response => response.json())
+        .then(data =>
+            data.forEach(repository => {
+                // Delete repositories without demo and Portfolio repository.
+                if (repository.homepage && repository.name != "Portfolio") {
+                    switch (filter) {
+                        case "all":
+                            insert(repository)
+                            break
+                        case "mobile-development":
+                            // Filter Mobile Development repositories.
+                            if (repository.topics[0] == "mobile-development") {
+                                insert(repository)
+                            }
 
-            html += `<span>
-            <a href="${item.url}" target="_blank"><img src="/images/github.png">GitHub</a>`
+                            break
+                        case "web-design-and-development":
+                            // Filter Web Design & Development repositories.
+                            if (repository.topics[0] == "web-design-and-development") {
+                                insert(repository)
+                            }
 
-            if (item.homepage) {
-                html += `<a href="${item.homepage}" target="_blank"><img src="/images/desktop-monitor.png">Demo</a>`
-            }
+                            break
+                    }
+                }
+            })
+        )
+}
 
-            switch (item.language) {
-                case "CSS":
-                    html += `<a class="language"><img src="/images/css-3.png">${item.language}</a>`
-                    break;
-                case "JavaScript":
-                    html += `<a class="language"><img src="/images/java-script.png">${item.language}</a>`
-                    break;
-                case "EJS":
-                    html += `<a class="language"><img src="/images/ejs_icon_132422.png">${item.language}</a>`
-                    break;
-                case "Handlebars":
-                    html += `<a class="language"><img src="/images/handlebars_original_logo_icon_146483.png">${item.language}</a>`
-                    break;
-                case "HTML":
-                    html += `<a class="language"><img src="/images/html.png">${item.language}</a>`
-                    break;
-                case "Kotlin":
-                    html += `<a class="language"><img src="/images/pngwing.com.png">${item.language}</a>`
-                    break;
-                case "Swift":
-                    html += `<a class="language"><img src="/images/swift.png">${item.language}</a>`
-            }
+function insert(repository) {
+    html = `<section>
+    <h3>${repository.name}</h3>`
 
-            html += `</span>
-            </section>`
-            
-            // Add the repositories to the index page.
-            $("section").insertAdjacentHTML("beforeend", html)
-        })
-    )
+    if (repository.description) {
+        html += `<p>${repository.description}</p>`
+    }
+
+    html += `<span>
+    <a href="${repository.url}" target="_blank"><img src="/images/github.png">GitHub</a>`
+
+    if (repository.homepage) {
+        html += `<a href="${repository.homepage}" target="_blank"><img src="/images/desktop-monitor.png">Demo</a>`
+    }
+
+    switch (repository.language) {
+        case "CSS":
+            html += `<a class="language"><img src="/images/css-3.png">${repository.language}</a>`
+            break
+        case "JavaScript":
+            html += `<a class="language"><img src="/images/java-script.png">${repository.language}</a>`
+            break
+        case "EJS":
+            html += `<a class="language"><img src="/images/ejs_icon_132422.png">${repository.language}</a>`
+            break
+        case "Handlebars":
+            html += `<a class="language"><img src="/images/handlebars_original_logo_icon_146483.png">${repository.language}</a>`
+            break
+        case "HTML":
+            html += `<a class="language"><img src="/images/html.png">${repository.language}</a>`
+            break
+        case "Kotlin":
+            html += `<a class="language"><img src="/images/pngwing.com.png">${repository.language}</a>`
+            break
+        case "Swift":
+            html += `<a class="language"><img src="/images/swift.png">${repository.language}</a>`
+    }
+
+    html += `</span>
+    </section>`
+    
+    // Add the repositories to the index page.
+    $("section").insertAdjacentHTML("beforeend", html)
+}
